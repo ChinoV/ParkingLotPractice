01a88a0951e3e1c1d35abe84e684ce3327de08d2
Alfredo <chinochinese2@gmail.com>
2016-09-07 23:46:07 -0600

    vx

--- ParkingLotPractice/ParkingLot.cs
+++ ParkingLotPractice/ParkingLot.cs
@@ -45,10 +45,8 @@ namespace ParkingLotPractice
         }
 
         /// <summary>
-        /// 
+        /// Gives the range to GetEmptySlot method and returns the slot as an int
         /// </summary>
-        /// <param name="y"></param>
-        /// <param name="UserType"></param>
         public int GetParkingSlot(int UserType)
         {
             int slot = -1;
@@ -73,6 +71,9 @@ namespace ParkingLotPractice
             return slot;
         }
 
+        /// <summary>
+        /// returns True if it successfully inserts a Vehicle into a ParkingLot array, else returns False
+        /// </summary>
         public bool InsertVehicle(Vehicle y, int slot)
         {
             if (slot != -1)
@@ -86,11 +87,10 @@ namespace ParkingLotPractice
             }
 
         }
+
         /// <summary>
-        /// 
+        /// Validates that License Plates dont repeat
         /// </summary>
-        /// <param name="ULicense"></param>
-        /// <returns></returns>
         public bool LicensePlatesExists(string ULicense)
         {
             for (int i = 0; i < ParkingLotArray.Length; i++)
@@ -108,15 +108,160 @@ namespace ParkingLotPractice
             return false;
         }
 
-    
-  //else
-  //                      {
-  //                          Console.Writeline("We already have a vehicle with that license plate")
-  //                      }
+        /// <summary>
+        /// Displays license plates in parking lot array
+        /// </summary>
+        /// <returns></returns>
+        public string LPReport()
+        {
+            string Report= "\nLicense Plates report: \n";
+            for (int i = 0; i < ParkingLotArray.Length; i++)
+            {
+                if (ParkingLotArray[i]!=null && !ParkingLotArray[i].LicensePlate.Equals(""))
+                {
+                    Report += i+": "+ParkingLotArray[i].LicensePlate+"\n";
+                }
+
+                else
+                {
+                    Report += i + ": No vehicle in this slot \n";
+                }
+                
+            }
+            return Report;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string IndividualEarningsReport()
+        {
+            string Report = "\nCost report: \n";
+            for (int i = 0; i < ParkingLotArray.Length; i++)
+            {
+                if (ParkingLotArray[i] != null && ParkingLotArray[i].Cost!= 0)
+                {
+                    Report += i + ": " + ParkingLotArray[i].Cost + "\n";
+                }
+
+                else
+                {
+                    Report += i + ": No vehicle in this slot \n";
+                }
+
+            }
+            return Report;
+        }
+
+        /// <summary>
+        /// Sums up all cost values in array, giving a total.
+        /// </summary>
+        /// <returns></returns>
+        public string TotalEarningsReport()
+        {
+            string TxtTotal = "\nTotal produced: \n";
+            int numTotal = 0;
+            for (int i = 0; i < ParkingLotArray.Length; i++)
+            {
+                if (ParkingLotArray[i] != null && ParkingLotArray[i].Cost != 0)
+                {
+                    numTotal += ParkingLotArray[i].Cost;
+                }
+                
+            }
+            TxtTotal += numTotal+"\n";
+            return TxtTotal;
+        }
+
+        /// <summary>
+        /// Returns how many slots are available in array between begin variable and end variable
+        /// </summary>
+        /// <returns></returns>
+        public int AmountEmptyPerType(int begin, int end)
+        {
+            int numTotal = 0;
+
+            for (int i = begin; i <= end; i++)
+            {
+                if (ParkingLotArray[i] == null)
+                {
+                    numTotal++;
+                }
+
+            }
+            return numTotal;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string EmptySlotsTypes()
+        {
+            return "\nSpots available for " +
+                "\nMotorcycles: " + AmountEmptyPerType(0, 3) +
+                "\nCars: " + AmountEmptyPerType(4, 8) +
+                "\nVans: " + AmountEmptyPerType(9, 14) +
+                "\nBuses: " + AmountEmptyPerType(15, 20) +
+                "\nTrucks: " + AmountEmptyPerType(21, 27);
+        }
 
 
-    #endregion
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public string TotalEmptySlots()
+        {
+            string TxtTotal = "\nTotal spots available: \n";
+            int numTotal = 0;
+            for (int i = 0; i < ParkingLotArray.Length; i++)
+            {
+                if (ParkingLotArray[i] == null)
+                {
+                    numTotal++;
+                }
+                
+            }
+            TxtTotal += numTotal+"\n";
+            return TxtTotal;
+        }
+
+        /// <summary>
+        /// Method with the report menu switch
+        /// </summary>
+        public void ReportMenu()
+        {
+            int UserType;
+            Console.WriteLine("Press \n 1: for License Plates Report \n 2: for Earnings Report \n 3: for Empty Slots");
+
+            int.TryParse(Console.ReadKey().KeyChar.ToString(), out UserType);
+
+            if (UserType != 0)
+            {
+                switch (UserType)
+                {
+                    case 1:
+                        Console.WriteLine(LPReport());
+                        break;
+                    case 2:
+                        Console.WriteLine(IndividualEarningsReport());
+                        Console.WriteLine(TotalEarningsReport());
+                        break;
+                    case 3:
+                        Console.WriteLine(EmptySlotsTypes());
+                        Console.WriteLine(TotalEmptySlots());
+                        break;
+                    default:
+                        Console.WriteLine("\n Input error");
+                        break;
+                }
+            }
+        }
 
 
-}
+        #endregion
+
+
+    }
 }
--- ParkingLotPractice/Program.cs
+++ ParkingLotPractice/Program.cs
@@ -18,67 +18,85 @@ namespace ParkingLotPractice
 
             while (UserType!=0)
             {
-                Console.WriteLine("Press \n 1: for Motorcycle \n 2: for Car \n 3: for Van \n 4: for Bus \n 5: for Truck\nPress anything other key to exit.");
-
-                int.TryParse(Console.ReadKey().KeyChar.ToString(), out UserType);
-
-                if (UserType != 0)
+                Console.WriteLine("Press \n 1: for Motorcycle \n 2: for Car \n 3: for Van \n 4: for Bus \n 5: for Truck\n 6: To enter the Reports Menu\n Press 0 to exit.\n");
+                if (int.TryParse(Console.ReadKey().KeyChar.ToString(), out UserType) == true)
                 {
-                    switch (UserType)
+                    if (UserType != 0)
                     {
-                        case 1:
-                            y = new Motorcycle();
-                            Console.WriteLine("\nYou want to park a Motorcycle, the cost is " + y.Cost);
-                            break;
-                        case 2:
-                            y = new Car();
-                            Console.WriteLine("\nYou want to park a Car, the cost is " + y.Cost);
-                            break;
-                        case 3:
-                            y = new Van();
-                            Console.WriteLine("\nYou want to park a Van, the cost is " + y.Cost);
-                            break;
-                        case 4:
-                            y = new Bus();
-                            Console.WriteLine("\nYou want to park a Bus, the cost is " + y.Cost);
-                            break;
-                        case 5:
-                            y = new Truck();
-                            Console.WriteLine("\nYou want to park a Motorcycle, the cost is " + y.Cost);
-                            break;
-                        default:
-                            Console.WriteLine("\n Input error");
-                            break;
-                    }
-                }
-
-                if (y!=null)
-                {
-                    int slot = PLot.GetParkingSlot(UserType);
-                    Console.WriteLine("\nWe have spots available. Please type in your license plate, can't be longer than 8 characters");
-
-                    License = Console.ReadLine().ToString();
-                    while (License.Length > 8)
-                    {
-                        Console.WriteLine("License plate too long, must be shorter than 8 characters\nType in license plate:");
-                        License = Console.ReadLine().ToString();
-                    }
-                    if (PLot.LicensePlatesExists(License) != true)
-                    {
-                        y.LicensePlate = License;
-                        PLot.InsertVehicle(y, UserType);
-                        Console.WriteLine("Your vehicle has been parked");
+                        switch (UserType)
+                        {
+                            case 1:
+                                y = new Motorcycle();
+                                Console.WriteLine("\nYou want to park a Motorcycle, the cost would be " + y.Cost);
+                                break;
+                            case 2:
+                                y = new Car();
+                                Console.WriteLine("\nYou want to park a Car, the cost would be " + y.Cost);
+                                break;
+                            case 3:
+                                y = new Van();
+                                Console.WriteLine("\nYou want to park a Van, the cost would be " + y.Cost);
+                                break;
+                            case 4:
+                                y = new Bus();
+                                Console.WriteLine("\nYou want to park a Bus, the cost would be " + y.Cost);
+                                break;
+                            case 5:
+                                y = new Truck();
+                                Console.WriteLine("\nYou want to park a Truck, the would be " + y.Cost);
+                                break;
+                            case 6:
+                                Console.WriteLine("\n");
+                                PLot.ReportMenu();
+                                break;
+                            default:
+                                Console.WriteLine("\n Input error");
+                                break;
+                        }
                     }
 
-                       
-                       
+                    if (UserType != 6)
+                    {
+                        if (y != null)
+                        {
+                            int slot = PLot.GetParkingSlot(UserType);
+                            if (slot != -1)
+                            {
+                                Console.WriteLine("\nPlease type in your license plate to proceed, minimum of 1 character and a maximum of 8 characters. ");
+                                License = Console.ReadLine().ToString();
+                                while ((License.Length > 8) || License.Length < 1)
+                                {
+                                    Console.WriteLine("    Invalid License Plate. Must be longer than 1 character and shorter than 8.\nType in license plate:");
+                                    License = Console.ReadLine().ToString();
+                                }
+                                if (PLot.LicensePlatesExists(License) != true)
+                                {
+                                    y.LicensePlate = License;
+                                    PLot.InsertVehicle(y, slot);
+                                    Console.WriteLine("\nYour vehicle has been parked\n");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("\n   License plate already exixts.\n");
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("    But we have no available spots for this type of vehicle, please select a different type\n");
+                            }
+                        }
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("No available spots for this type of vehicle, please select a different type");
+                    Console.WriteLine("\nInput error");
+                    UserType = -1;
                 }
+                 
             }
         }
     }
 }
+
 
